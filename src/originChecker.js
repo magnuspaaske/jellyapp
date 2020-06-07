eed12b2ce804e0ba77fc43d5119879781f441c79
Magnus Petersen-Paaske <magnuspetersenpaaske@gmail.com>
2020-06-07 11:07:47 +0200

    Updated origin checker to accept multiple origins (for tests online etc)

--- src/originChecker.js
+++ src/originChecker.js
@@ -1,9 +1,14 @@
 // Middleware to redirect to page origin domain
 
+const _ = require('lodash')
+
 module.exports = (app) => {
     app.use((req, res, next) => {
-        if (`${req.protocol}://${req.get('host')}` !== process.env.PAGE_ORIGIN) {
-            return res.redirect(`${process.env.PAGE_ORIGIN}${req.url}`)
+        // Split page origin in case there's multiple
+        const origins = process.env.PAGE_ORIGIN.split(/[,,;]/)
+
+        if (!_(origins).includes(`${req.protocol}://${req.get('host')}`)) {
+            return res.redirect(origins[0])
         }
 
         next()
