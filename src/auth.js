431fd7c115d9b941bb4b0a183c5cfc57928f8970
Magnus Petersen-Paaske <magnuspetersenpaaske@gmail.com>
2020-12-08 11:46:52 +0100

    Changed auth to giving 401 errors

--- src/auth.js
+++ src/auth.js
@@ -10,13 +10,13 @@ module.exports = (resources = {}) => {
 
         // Check that there is a user
         if (!session.user) {
-            throw new APIError(403, 'You must be logged in to view this page')
+            throw new APIError(401, 'You must be logged in to view this page')
         }
 
         // Check that there's an admin
         if (resources.admin) {
             if (session.user.get('is_admin') !== true) {
-                throw new APIError(403, 'You must have admin privileges to view this page')
+                throw new APIError(401, 'You must have admin privileges to view this page')
             }
         }
 
